9667d4362a054a0fc62193913201137421cec552
Vinicius José <vinicius.jsilv@gmail.com>
2021-05-25 17:21:15 -0300

    Inserçao de botao de direcionamento para o dashboard

--- www/views/Page404.php
+++ www/views/Page404.php
@@ -1,4 +1,8 @@
 <main>
-    <h2>Oooops ! Parece que algo deu errado</h2>
-    <h3>Erro : 404(Página não encontrada)</h3>
+    <div class="error404">
+        <h2>Oooops ! Parece que algo deu errado</h2>
+        <h3>Erro : 404 (Página não encontrada)</h3><br>
+        <a href="<?php echo BASE_URL?>" class="btn-action">Voltar para o Dashboard</a>
+        <br><br><br>
+    </div>
 </main>
