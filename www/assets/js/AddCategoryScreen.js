278223468a099486c536b35dd5cb3009758890c5
Vinicius José <vinicius.jsilv@gmail.com>
2021-05-28 15:44:57 -0300

    Separacao dos scripts da pagina de insercao de dados e visualizacao dos dados

---
+++ www/assets/js/AddCategoryScreen.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    /**
+     * Captura do envio de submit do formulário #form-add-category
+     * e chamada da função que realiza a persistência dos dados de
+     * categoria.
+     */
+    $('#form-add-category').on('submit', function(e){
+        e.preventDefault();
+        var codCat = $('#category-code').val();
+        var nameCat = $('#category-name').val();
+        insertCategory(codCat,nameCat)
+    });
+});
+/**
+ * Requisição ajax com dados das categorias.
+ * 
+ * Função que executa a requisição ajax com os dados inseridos pelo usuário 
+ * para o método addCategory no arquivo CategoryController.
+ * 
+ * @param {*} codCat 
+ * @param {*} nameCat 
+ */
+function insertCategory(codCat, nameCat){
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8000/Category/addCategory",
+        dataType:'json',
+        data: {
+            'category-name':nameCat,
+            'category-code':codCat
+        },
+        success: function (json) {
+            if(json == true){
+                $('#form-add-category').trigger("reset");
+                $('#alert').addClass('success-msg');
+                $('#alert').fadeIn().html('Categoria cadastrada com sucesso.');
+                setTimeout(function(){
+                    $('#alert').fadeOut('Slow')
+                    $('#alert').fadeIn().html('');
+                    $('#alert').removeClass('success-msg');
+                }, 5000)
+            }else{
+                $('#alert').addClass('alert-msg');
+                $('#alert').fadeIn().html('Erro ao cadastrar (Nome/Código de categorias duplicados.)');
+                setTimeout(function(){
+                    $('#alert').fadeOut('Slow')
+                    $('#alert').fadeIn().html('');
+                    $('#alert').removeClass('alert-msg');
+                }, 5000)
+            }
+        }
+    });
+}
