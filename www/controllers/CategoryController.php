95cf43870dc53d2c9897aa9a07d5ffcf9b48fd8e
Vinicius José <vinicius.jsilv@gmail.com>
2021-05-28 11:28:42 -0300

    Documentaçao da classe CategoryController e seus metodos

--- www/controllers/CategoryController.php
+++ www/controllers/CategoryController.php
@@ -1,5 +1,19 @@
 <?php
+    /**
+     * Realiza todas as implementações das paginas relacionadas a categorias.
+     * 
+     * Classe responsável por conter toda a regra de negocio da implementação de
+     * categorias no sistema.
+    **/
     class CategoryController extends Controller{
+        /**
+         * Página Principal da aplicação
+         *
+         * Método responsável por realizar a chamada do template e desenhar a pagina inicial
+         * de categorias.
+         *
+         * @param Array $data Todos os dados que serão fornecidos para o front-end da aplicação
+         **/
         public function index(){
             $data = [
                 'page' => 'Categorias',
@@ -7,6 +21,14 @@
             ];
             $this->loadTemplate('Categories', $data);
         }
+        /**
+         * Página de adição de novas categorias da aplicação
+         *
+         * Método responsável por realizar a chamada do template e desenhar a pagina de adição
+         * de novas categorias no sistema
+         *
+         * @param Array $data Todos os dados que serão fornecidos para o front-end da aplicação
+         **/
         public function addScreen(){
             $data = [
                 'page' => 'Adicionar Categorias',
@@ -14,6 +36,18 @@
             ];
             $this->loadTemplate('AddCategory', $data);
         }
+        /**
+         * Método de persistência de novas categorias ao banco de dados.
+         *
+         * Método responsável por receber a requisição ajax do script Category.js, tratar as informações
+         * e requisitar a função (insertCategory) de inserção de categoria ao banco de dados que esta localizada
+         * na pasta models/CategoryRepository.php
+         *
+         * @param Boolean $status Responsável pelo retorno para o front-end.
+         * @param String $name Nome da categoria recebida pela requisição ajax
+         * @param String $cod Código da categoria recebida pela requisição ajax
+         * @return Boolean
+         **/
         public function addCategory(){
             $status = true;
             $cateRepo = new CategoryRepository();
