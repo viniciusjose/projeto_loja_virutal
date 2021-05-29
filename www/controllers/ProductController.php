ae853744b6592b2549dcaae94885a34a3bfdab0e
Vinicius José <vinicius.jsilv@gmail.com>
2021-05-29 20:05:46 -0300

    Documentacao do arquivo ProductController

--- www/controllers/ProductController.php
+++ www/controllers/ProductController.php
@@ -1,5 +1,16 @@
 <?php
+    /**
+     * Classe responsável por todas as requisições de produtos do sistema.
+     */
     class ProductController extends Controller{
+        /**
+         * Método de renderização da página principal de produtos.
+         *
+         * Responsável por requisitar para a camada view, o conteúdo a ser exibido
+         * na tela de Produtos do sistema.
+         *
+         * @var Array $data Informações que serão inseridas na view Products.html
+         **/
         public function index(){
             $data = [
                 'title' => 'Produtos',
@@ -8,6 +19,14 @@
             ];
             $this->loadTemplate('Products', $data);
         }
+        /**
+         * Método de renderização da página de adicionar produtos.
+         *
+         * Responsável por requisitar para a camada view o conteúdo a ser exibido
+         * na tela adicionar novos produtos no sistema.
+         *
+         * @var Array $data Informações que serão inseridas na view AddProduct.html
+         **/
         public function addScreen(){
             $data = [
                 'title' => 'Adicionar Produtos',
@@ -16,6 +35,24 @@
             ];
             $this->loadTemplate('AddProduct', $data);
         }
+        /**
+         * Método de renderização da página de Logs
+         *
+         * Responsável por requisitar para a camada view o conteúdo a ser exibido
+         * na tela de Logs do sistema.
+         *
+         * @var Boolean $status Status da transação com o banco de dados para ser
+         * retornado em JSON para a requisição AJAX
+         * @var Object $prodRepo Objeto instanciado da classe ProductRepository.
+         * @var Object $image Objeto instanciado da classe ImageController.
+         * @var String $sku Código SKU do produto.
+         * @var String $name Nome do Produto.
+         * @var Float $price Preço do produto.
+         * @var Array $category Todas as categorias selecionadas para o produto.
+         * @var Integer $quantity Quantidade de produtos em estoque.
+         * @var String $image Caminho do arquivo onde a imagem foi salva.
+         * @var String $description Descrição completa do produto.
+         **/
         public function addProduct(){
             $status = true;
             $prodRepo = new ProductRepository();
@@ -27,8 +64,6 @@
             (isset($_POST['quantity']) && !empty($_POST['quantity'])) &&
             (isset($_POST['category']) && !empty($_POST['category'])) &&
             (isset($_POST['description']) && !empty($_POST['description']))) {
-                    
-                $product = new Product();
                 
                 $sku = addslashes(strtoupper($_POST['sku']));
                 $name = addslashes(ucwords($_POST['name']));
