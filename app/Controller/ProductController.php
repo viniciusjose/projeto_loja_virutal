904ef6852b02ff0930054042d544094e2675cf03
Vinicius José <vinicius.jsilv@gmail.com>
2021-05-31 21:37:59 -0300

    Alteracao nas permissoes de escrita e leitura das pastas

--- app/Controller/ProductController.php
+++ app/Controller/ProductController.php
@@ -66,35 +66,26 @@
             $status = true;
             $prodRepo = new ProductRepository();
             $image = new ImageController();
-            /**
-             * verificação dos campos digitados pelo usuário e inserção
-             * do produto ao banco de dados.
-             */
-            if ((isset($_POST['sku']) && !empty($_POST['sku'])) &&
-            (isset($_POST['name']) && !empty($_POST['name'])) &&
-            (isset($_POST['price']) && !empty($_POST['price'])) &&
-            (isset($_POST['quantity']) && !empty($_POST['quantity'])) &&
-            (isset($_POST['category']) && !empty($_POST['category'])) &&
-            (isset($_POST['description']) && !empty($_POST['description']))) {
+        
                 
-                $sku = addslashes(strtoupper($_POST['sku']));
-                $name = addslashes(ucwords($_POST['name']));
-                $price = addslashes(
-                    floatval(str_replace(",", ".", $_POST['price']))
-                );
-                $category = $_POST['category'];
-                $quantity = addslashes(intval(intval($_POST['quantity'])));
-                $image = $_POST['category'];
-                $description = addslashes(ucfirst($_POST['description']));
+            $sku = addslashes(strtoupper($_POST['sku']));
+            $name = addslashes(ucwords($_POST['name']));
+            $price = addslashes(
+                floatval(str_replace(",", ".", $_POST['price']))
+            );
+            $category = $_POST['category'];
+            $quantity = addslashes(intval(intval($_POST['quantity'])));
+            $image = $_POST['category'];
+            $description = addslashes(ucfirst($_POST['description']));
 
-                /**Função upload é executada para pegar a imagem selecionada 
-                 * pelo usuário e retornar o caminho da imagem
-                 */
-                $imageDirectory = $image->AddProductImage();
-                if ($prodRepo->createProduct($sku, $name, $price, $description,
-                 $imageDirectory, $quantity, $category)) {
-                    return json_encode($status);
-                }
+            /**Função upload é executada para pegar a imagem selecionada 
+             * pelo usuário e retornar o caminho da imagem
+             */
+            $imageDirectory = $image->AddProductImage();
+            if ($prodRepo->createProduct($sku, $name, $price, $description,
+                $imageDirectory, $quantity, $category)) {
+                return json_encode($status);
+            }
             }
         }
     }
--- app/Model/ProductRepository.php
+++ app/Model/ProductRepository.php
@@ -9,15 +9,36 @@
         /**
          * Inserção de produto.
          *
-         * Função responsável por receber os dados do formulário de produto e adicionar ao banco de dados.
+         * Função responsável por receber a requisição da classe controller e
+         * inserir um novo produto ao banco de dados.
          * 
-         * @param Type $skuProd, $name_prod, $price_prod, $desc_prod, $image_prod, $qtd_prod, $id_categorias
-         * @return type boolean
+         * @param String $skuProd
+         * @param String $name_prod
+         * @param Float $price_prod
+         * @param String $desc_prod
+         * @param String $image_prod
+         * @param Integer $qtd_prod
+         * @param Array $id_categories
+         * @return Boolean boolean
          **/
-        public function insertProduct($skuProd, $name_prod, $price_prod, $desc_prod, $image_prod, $qtd_prod, $id_categorias)
+        public function insertProduct($skuProd, $name_prod, $price_prod, $desc_prod, 
+        $image_prod, $qtd_prod, $id_categories)
         {
-            $id_product;
-            $sql = "INSERT INTO product (sku, name_product, price, description_product, image_product, quantity) VALUES (:sku, :name_product, :price, :description_product, :image_product, :quantity)";
+            $sql = "INSERT INTO product (
+                sku, 
+                name_product, 
+                price, 
+                description_product, 
+                image_product, 
+                quantity
+            ) 
+            VALUES (
+                :sku, :name_product, 
+                :price, 
+                :description_product, 
+                :image_product,
+                :quantity
+            )";
             $sql = $this->db->prepare($sql);
             $sql->bindValue(":sku",$skuProd);
             $sql->bindValue(":name_product",$name_prod);
@@ -26,19 +47,26 @@
             $sql->bindValue(":image_product",$image_prod);
             $sql->bindValue(":quantity",$qtd_prod);
             $sql->execute();
-            $id_prod = intval($database->lastInsertId());
-            
-            for($i = 0; $i < count($id_categorias); $i++){
+            $id_prod = intval($database->lastInsertId());    
+            $this->insertCategoryInProduct($id_prod, $id_categories);
+            return true;
+        }
+        private insertCategoryInProduct($id_product, $id_categories)
+        {
+            for ($i = 0; $i < count($id_categories); $i++) {
 
                 try {
-                    $query = "INSERT INTO product_category (id_category, id_product) VALUES ('$id_categorias[$i]','$id_prod')";
-                    $query = $database->query($query);
+                    $sql = "INSERT INTO product_category (id_category, id_product) 
+                        VALUES (
+                            '$id_categories[$i]',
+                            '$id_prod'
+                        )
+                    ";
+                    $sql = $this->db->query($query);
                 } catch (PDOException $e) {
                     echo "Erro: ".$e->getMessage();
-               }
+                }
             }
-            
-            return true;
         }
         
     }
